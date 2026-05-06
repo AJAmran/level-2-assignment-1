0e2d727416e74c6738519b30dbc1c8932706f063
AJAmran <mdamranhossen77@gmail.com>
2026-05-06 23:52:50 +0600

    solved problem 6 and 7

--- solutions.ts
+++ solutions.ts
@@ -24,3 +24,33 @@ const checkType = (value: StringOrNumber): StringOrNumber => {
 const getProperty = <T, K extends keyof T>(obj: T, key: K): T[K] => {
   return obj[key];
 };
+
+//todo: Problem 6
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Student extends Person {
+  grade: string;
+
+  constructor(name: string, age: number, grade: string) {
+    super(name, age);
+    this.grade = grade;
+  }
+
+  getDetails() {
+    return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+  }
+}
+
+
+//todo: Problem 7
+const getIntersection = (arr1: number[], arr2: number[]): number[] => {
+  return arr1.filter((item) => arr2.includes(item));
+};
