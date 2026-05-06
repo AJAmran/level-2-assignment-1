bb9bdffc0d76184ef3532cb391b999f49a2d3254
AJAmran <mdamranhossen77@gmail.com>
2026-05-06 23:00:46 +0600

    solved problem 4

--- solutions.ts
+++ solutions.ts
@@ -9,3 +9,18 @@ const reverseString = (str: string): string => {
   return str.split("").reverse().join("");
 };
 
+//todo: Problem 3
+type StringOrNumber = string | number;
+
+const checkType = (value: StringOrNumber): StringOrNumber => {
+  if (typeof value === "string") {
+    return "String";
+  } else {
+    return "Number";
+  }
+};
+
+//todo: Problem 4
+const getProperty = <T, K extends keyof T>(obj: T, key: K): T[K] => {
+  return obj[key];
+};
